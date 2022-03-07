d202fe3283f1e8b920326a665b702d83f7ee6ea0
krystazapatero <84065504+krystazapatero@users.noreply.github.com>
2022-03-07 14:05:13 +0800

    Add files via upload

---
+++ image.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gallery</title>
+	<link rel="stylesheet" type="text/css" href="imageStyles.css">
+     
+	
+</head>
+<body style="text-align: center;">
+
+  	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+     <div class="nav-bar">
+      <nav class="navbar navbar-expand-lg ">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+           aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                  <a class="nav-link" href="resume.php">MY RESUME</a>
+                </li>
+              <li class="nav-item">
+                <a class="nav-link " href="#">MY GALLERY</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="index.php">SIGN OUT</a>
+              </li>
+            </ul>
+          </div>
+      </nav>
+  </div><br><br>
+<h1>GALLERY</h1><br><br>
+
+	<div class="pic">
+		<a href="folder1/kj1.jpg"><img src="folder1/kj1.jpg"></a>
+		<a href="folder1/kj2.jpg"><img src="folder1/kj2.jpg"></a>
+    <a href="folder1/kj3.jpeg"><img src="folder1/kj3.jpeg"></a>
+    <a href="folder1/kj4.jpeg"><img src="folder1/kj4.jpeg"></a>
+    <a href="folder1/kj5.jpg"><img src="folder1/kj5.jpg"></a>
+		
+
+		<br><br><br>
+
+</div><br><br>
+</body>
+</html>
---
+++ resume.php
@@ -0,0 +1,154 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>MY RESUME</title>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <body>
+       
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <link rel="stylesheet" href="resumeStyles.css">
+  
+     <div class="nav-bar">
+      <nav class="navbar navbar-expand-lg ">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+           aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                  <a class="nav-link" href="resume.php">MY RESUME</a>
+                </li>
+              <li class="nav-item">
+                <a class="nav-link " href="image.php">MY GALLERY</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="index.php">SIGN OUT</a>
+              </li>
+            </ul>
+          </div>
+      </nav>
+  </div> 
+
+     <section id="header">
+        <div class="container text-center">
+         <div class="user-box">
+             <img src="folder1/kj1.jpg" >
+             <h1>Zapatero, Krysta Jane M.</h1>
+             <b>System Administration & Maintenance<br>C187-C188</b>
+        </div>
+        </div>
+        
+    </section>
+    <section>
+   
+
+    <div class="about container" id="about">
+    <div class="row">
+   
+
+    </div>
+    <div class="col-md-50" style="margin-left: 00px; margin-top: 0px;">
+        <h3><u>ABOUT</u></h3>
+        <p> My name is Krysta Jane but you can call me Krysta. I'm currently student in Urdaneta City University and my course is about technology, Bachelor of Science and Information Technology. I am also the youngest daughter in our family. My mother is a full time housewife and my father is a driver.
+
+It is a common knowledge that I am a good student and I love to study a lot.I’m what a lot of people call a geek. I have every intention of applying to a reputable university and obtaining a degree in one of these fields. I am responsible and hardworking.
+        </p>
+    
+
+
+<div class="resume" id="resume">
+<div class="container">
+<div class="row">
+<div style="margin-left: -120px;">
+<h3 class="text-center"><b><u> MY EXPERIENCES </u></b></h3>
+<ul class="timeline">
+<li>
+ 
+
+<li>
+    <h4><b>2020-2021  Web Developer</b></h4> 
+    <p>Creating website layout/user interfaces by using standard HTML/CSS practices.<br>
+  </p> 
+  </li>
+
+  <li>
+ <h4><b>2019-2020 - Programming</b></h4> 
+ <p> by creating a proggram or a system i used different application like C++, Netbeans, and my SQL, and java.<br><br><br>
+ </p> 
+</li>
+</ul>
+
+</div>
+
+
+
+<div style="margin-left: -120px;">
+    <h3 class="text-center"><b><u> EDUCATIONAL BACKGROUND </u></b></h3>
+    <ul class="timeline">
+    <li>
+      <h4><b>Urdaneta City University</b></h4> 
+      <p> 
+          Year Graduated: SY 2020 - Present <br>Address: San Vincente East Urdaneta City University
+    </p> 
+    </li>
+    
+    <li>
+        <h4><b>ABE International College</b></h4>
+        <p>
+            Year Graduated: SY 2018 - 2019 <br>Address: Marcos Highway Urdaneta City Pangasinan</p>
+      </p> 
+      </li>
+    
+      <li>
+        <h4><b>Pallas-Papagueyan Elementary School</b></h4> 
+        <p> 
+            Year Graduated: SY 2012 - 2013 Address: Tebag West Sta.Barbara Pangasinan
+      </p> 
+    </li>
+    </ul>
+    
+    </div>
+
+    
+<div class="CI" style="margin-left: 190px;">
+  
+    <h3 class="text-center"><b><u>CONTACT INFORMATION </u></b></h3>
+    <ul class="timeline">
+    <li>
+      <h4><b>Facebook</b></h4> 
+      <p> 
+          Krysta Zapatero
+    </p> 
+    </li>
+    
+    <li>
+        <h4><b>Gmail Account</b></h4> 
+        <p>
+             jankey444@gmail.com
+      </p> 
+      </li>
+    
+      <li>
+        <h4><b>Contact Number</b></h4> 
+        <p> 
+             09771322507 <br>
+      </p> 
+    </li>
+    </ul>
+    
+</div>
+
+</div>
+</div>
+</div>
+
+
+
+</body>
+</html>
